7699a14a09894503531325aa4d0a16fb7c3aa40e
André Albson <106167607+albsondev@users.noreply.github.com>
2024-10-24 14:07:25 -0300

    add sass + layout/design + remove junk files
    add sass + layout/design + remove junk files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ControlPanel from './components/ControlPanel';
+import Typography from '@mui/material/Typography';
+import CurtainsClosedIcon from '@mui/icons-material/CurtainsClosed';
+import Divider from '@mui/material/Divider';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Controle de Volume de Armazenamento</h1>
+    <div className='container' style={{ padding: '2rem' }}>
+      <Typography variant="h3" textAlign={'center'}>
+        <CurtainsClosedIcon sx={{ fontSize: 37 }} />
+        Controle de Volume de Armazenamento
+      </Typography>
+      <Divider className='divider-container' />
       <ControlPanel />
     </div>
   );
