c75d1c2ac77d8aab4ee0e4c2a8ad70dac39112d1
André Albson <106167607+albsondev@users.noreply.github.com>
2024-10-29 17:08:56 -0300

    remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ControlPanel from './components/ControlPanel';
-import Typography from '@mui/material/Typography';
-import CurtainsClosedIcon from '@mui/icons-material/CurtainsClosed';
-import Divider from '@mui/material/Divider';
 import './App.scss';
 
 const App: React.FC = () => {
